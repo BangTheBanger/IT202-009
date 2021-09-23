cb4bd00c170ab14daec8c03da989da8e81aa62b9
Filipe A <nullnight123@gmail.com>
2021-09-23 04:11:23 -0400

    Initial commit, just to save my progress. Currenly 5/9 Points

--- public_html/index.php
+++ public_html/index.php
@@ -1,11 +1,15 @@
-<?php echo "It works!\nFilipe Atanes' website, for IT202-009";?>
+<?php echo "It works!<br>Filipe Atanes' website, for IT202-009";?>
 <ul>
     <li><a href="index.php">Index Page</a></li>
     <li><h3>Homework 2 - Problems 1-3</h3>
     <ul>
-        <li><a href="HW2-Problem1.php">HW 2 - Problem 1</a>
-        <li><a href="HW2-Problem2.php">HW 2 - Problem 2</a>
-        <li><a href="HW2-Problem3.php">HW 2 - Problem 3</a>
+        <li><a href="HW2/HW2-Problem1.php">HW 2 - Problem 1</a>
+        <li><a href="HW2/HW2-Problem2.php">HW 2 - Problem 2</a>
+        <li><a href="HW2/HW2-Problem3.php">HW 2 - Problem 3</a>
+    </ul>
+    <li><h3>Homework 3 - JS & CSS Challenge</h3>
+    <ul>
+        <li><a href="CSS_&_Java_Challenge/Challenge.html">HW3 - JS & CSS Challenge</a></li>
     </ul>
     <li><h3>Homework X - Problems X-X</h3>
     <ul>
