bcd4c8a2729f7fb48427c7dc238b5a40f0846af4
Filipe A <nullnight123@gmail.com>
2021-12-17 02:41:59 -0500

    Working on MS4 now

--- public_html/Project/admin/assign_roles.php
+++ public_html/Project/admin/assign_roles.php
@@ -15,7 +15,7 @@ if (isset($_POST["users"]) && isset($_POST["roles"])) {
     } else {
         //for sake of simplicity, this will be a tad inefficient
         $db = getDB();
-        $stmt = $db->prepare("INSERT INTO UserRoles (user_id, role_id, is_active) VALUES (:uid, :rid, 1) ON DUPLICATE KEY UPDATE is_active = !is_active");
+        $stmt = $db->prepare("INSERT INTO user_roles (user_id, role_id, is_active) VALUES (:uid, :rid, 1) ON DUPLICATE KEY UPDATE is_active = !is_active");
         foreach ($user_ids as $uid) {
             foreach ($role_ids as $rid) {
                 try {
@@ -32,7 +32,7 @@ if (isset($_POST["users"]) && isset($_POST["roles"])) {
 //get active roles
 $active_roles = [];
 $db = getDB();
-$stmt = $db->prepare("SELECT id, name, description FROM Roles WHERE is_active = 1 LIMIT 10");
+$stmt = $db->prepare("SELECT id, name, description FROM roles WHERE is_active = 1 LIMIT 10");
 try {
     $stmt->execute();
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -50,8 +50,8 @@ if (isset($_POST["username"])) {
     if (!empty($username)) {
         $db = getDB();
         $stmt = $db->prepare("SELECT Users.id, username, (SELECT GROUP_CONCAT(name, ' (' , IF(ur.is_active = 1,'active','inactive') , ')') from 
-        UserRoles ur JOIN Roles on ur.role_id = Roles.id WHERE ur.user_id = Users.id) as roles
-        from Users WHERE username like :username");
+        user_roles ur JOIN roles on ur.role_id = roles.id WHERE ur.user_id = users.id) as rolelist
+        from users WHERE username like :username");
         try {
             $stmt->execute([":username" => "%$username%"]);
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
