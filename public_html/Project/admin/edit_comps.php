31ba9d0c05ab25be9e9089f4b2c2c0657c16826c
Filipe A <nullnight123@gmail.com>
2021-12-17 02:51:05 -0500

    Working on MS4 now

--- public_html/Project/admin/edit_comps.php
+++ public_html/Project/admin/edit_comps.php
@@ -1,34 +1,8 @@
 <?php
-    require(__DIR__ . "/../../partials/nav.php");
+    require(__DIR__ . "/../../../partials/nav.php");
     if (!has_role("admin")) {
         flash("You don't have permission to view this page", "warning");
         die(header("Location: $BASE_PATH" . "home.php"));
     }
     $db = getDB();
 ?>
-
-<head>
-    <style>
-        * {
-            box-sizing: border-box;
-        }
-        .column {
-            float: left;
-            width: 50%;
-            padding: 10px;
-        }
-
-        .row:after {
-            content: "";
-            display: table;
-            clear: both;
-        }
-        form {
-            text-align: left;
-            align-self: left;
-            align-items: left;
-            align-content: left;
-        }
-    </style>
-</head>
-
