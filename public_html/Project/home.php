d0aa72e24571a73398e3e6c1769491543011de68
Filipe A <nullnight123@gmail.com>
2021-12-19 22:05:07 -0500

    Working on MS4 now

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -10,7 +10,7 @@ if (!is_logged_in()) {
 if (is_logged_in()) {
     echo "Welcome home, " . get_username();
     //comment this out if you don't want to see the session variables
-    //echo "<pre>" . var_export($_SESSION, true) . "</pre>";
+    echo "<pre>" . var_export($_SESSION, true) . "</pre>";
 }
 ?>
 <?php
